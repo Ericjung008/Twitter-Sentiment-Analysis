a38467001614e93101b20279efb3312c48dbde31
Eric JaeHoon Jung <ericjung008@gmail.com>
2020-12-26 00:29:56 -0800

    Update TwitterKeys.py

--- py files/TwitterKeys.py
+++ py files/TwitterKeys.py
@@ -3,4 +3,4 @@
 CONSUMER_KEY = '''Enter Twitter consumer key'''
 CONSUMER_SECRET = '''Enter Twitter consumer secret'''
 ACCESS_TOKEN = '''Enter Twitter access token'''
-ACCESS_TOKEN_SECRET = 'Enter Twitter access token secret'
+ACCESS_TOKEN_SECRET = '''Enter Twitter access token secret'''
